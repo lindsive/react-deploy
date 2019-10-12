eefca96a9354796fe3b6c10fc46a7a2df9203739
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-10-12 11:05:32 -0400

    commit

---
+++ client/build/precache-manifest.a81d3e09f55e1185674f8cfd376049d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "47b5abdd5359d0c6e7b2",
+    "url": "/static/js/main.339cf448.chunk.js"
+  },
+  {
+    "revision": "8589cfe95446d3fd0a4e",
+    "url": "/static/js/2.1d07b520.chunk.js"
+  },
+  {
+    "revision": "47b5abdd5359d0c6e7b2",
+    "url": "/static/css/main.47f4087b.chunk.css"
+  },
+  {
+    "revision": "0b5136c2748f05f9a44f268acb5ad201",
+    "url": "/index.html"
+  }
+];
